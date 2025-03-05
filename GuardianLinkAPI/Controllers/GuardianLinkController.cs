e76859843735d1ffa8ac115dfacf3232f99823d5
JhonMartinez118 <142257945+JhonMartinez118@users.noreply.github.com>
2025-03-05 09:45:49 -0600

    Add files via upload

---
+++ GuardianLinkAPI/Controllers/GuardianLinkController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace GuardianLinkAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GuardianLinkController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { message = "Hello, World!" });
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] string value)
+        {
+            return Ok(new { receivedValue = value });
+        }
+    }
+}
